e08e22dd477c0a6da1ec53b4c1ce2a296a584d77
miaflowautomation-ctrl <malenstwoooo1@googlemail.com>
2025-09-30 14:50:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
 import io
 import csv
 from datetime import datetime
@@ -619,3 +620,4 @@ with col2:
     else:
 
         st.info("⏳ Waiting")
+
